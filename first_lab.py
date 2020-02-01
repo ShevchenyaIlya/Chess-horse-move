93f34eefedc68de34580b11272634bc64bc67f02
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-02-01 11:59:13 +0300

    Add infinit loop to handle base ivents that happends in active surface and init base position for window.

--- first_lab.py
+++ first_lab.py
@@ -1,6 +1,17 @@
 import pygame
+import os
+os.environ["SDL_VIDEO_WINDOW_POS"] = "400, 100"
 
-pygame.init()
-pygame.display.set_mode((600, 600))
+surface = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Chess knight move")
+pygame.init()
 
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    surface.fill((0, 0, 0))
+    pygame.display.flip()
